c3fffbb2bc59b8a7806a34596390fb0bbb819204
sriv0023 <156090505+sriv0023@users.noreply.github.com>
2024-01-23 21:11:25 -0500

    HTML and JS files attached here.

---
+++ js/index.js
@@ -0,0 +1,36 @@
+let a = 3;
+let b = 5;
+let c;
+let result =  `
+let a = 3;
+let b = 5;
+let c;
+---------
+a + b = ${a + b}
+a - b = ${a - b}
+a * b = ${a * b}
+a / b = ${a / b}
+a % b = ${a % b}
+a += b = ${a += b}
+a -= b = ${a -= b}
+a *= b = ${a *= b}
+a /= b = ${a /= b}
+a %= b = ${a %= b}
+a == b = ${a == b}
+a != b = ${a != b}
+a > b = ${a > b}
+a < b = ${a < b}
+!a && !c = ${!a && !c}
+!a || !c = ${!a || !c }
+`;
+console.log(result);
+
+
+let f_name = "Aaditya";
+let l_name = "Srivastava";
+let email = "sriv0023@algonquinlive.com";
+let output = `My name is ${f_name} ${l_name}. You can contact me at ${email}.`;
+console.log(output);
+
+
+
